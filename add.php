d710d693f4e4ddbddde1bff41120221f8b344e7f
ghines2002 <100923222+ghines2002@users.noreply.github.com>
2023-04-13 11:32:56 +0200

    Add files via upload

---
+++ add.php
@@ -0,0 +1,35 @@
+<?php
+	session_start();
+	include_once('connection.php');
+
+	if(isset($_POST['add'])){
+		$firstname = $_POST['firstname'];
+		$lastname = $_POST['lastname'];
+		$address = $_POST['address'];
+		$phone = $_POST['phone'];
+		$email = $_POST['email'];
+		$role = $_POST['role'];
+		$sql = "INSERT INTO members (firstname, lastname, address, phone, email, role) VALUES ('$firstname', '$lastname', '$address', '$phone', '$email', '$role')";
+
+		//use for MySQLi OOP
+		if($conn->query($sql)){
+			$_SESSION['success'] = 'Member added successfully';
+		}
+		///////////////
+
+		//use for MySQLi Procedural
+		// if(mysqli_query($conn, $sql)){
+		// 	$_SESSION['success'] = 'Member added successfully';
+		// }
+		//////////////
+		
+		else{
+			$_SESSION['error'] = 'Something went wrong while adding';
+		}
+	}
+	else{
+		$_SESSION['error'] = 'Fill up add form first';
+	}
+
+	header('location: index.php');
+?>
